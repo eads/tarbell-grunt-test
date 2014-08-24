832129f2daf6f663e70e914f886930f05d1d0f57
David Eads <davideads@gmail.com>
2014-08-23 20:51:36 -0400

    fun new things

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,6 @@
 module.exports = function(grunt) {
   grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
     less: {
       development: {
         options: {
@@ -8,19 +9,44 @@ module.exports = function(grunt) {
           optimization: 2
         },
         files: {
-          "css/main.css": "less/main.less"
+          "css/main.css": "src/less/main.less"
+        }
+      }
+    },
+    concat: {
+      options: {
+        // define a string to put between each file in the concatenated output
+        separator: ';'
+      },
+      dist: {
+        // the files to concatenate
+        src: ['src/js/**/*.js'],
+        // the location of the resulting JS file
+        dest: 'js/<%= pkg.name %>.js'
+      }
+    },
+    uglify: {
+      options: {
+        // the banner is inserted at the top of the output
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          'js/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
         }
       }
     },
     watch: {
       styles: {
-        files: ['less/**/*.less'], // which files to watch
-        tasks: ['less'],
+        files: ['src/less/**/*.less', 'src/js/**/*.js'], // which files to watch
+        tasks: ['less', 'concat', 'uglify'],
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.registerTask('default', ['less']);
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.registerTask('default', ['less', 'concat', 'uglify']);
 }
